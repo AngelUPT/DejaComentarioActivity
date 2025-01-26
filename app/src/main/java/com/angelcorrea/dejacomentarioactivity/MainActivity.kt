bc2b19ce25c2a0f4e4eacf33a8014c99c5682858
AngelUPT <angel_1322134008@uptecamac.edu.mx>
2025-01-26 13:37:57 -0600

    adding actions

--- app/src/main/java/com/angelcorrea/dejacomentarioactivity/MainActivity.kt
+++ app/src/main/java/com/angelcorrea/dejacomentarioactivity/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.angelcorrea.dejacomentarioactivity
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,30 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val nombreEditText = findViewById<EditText>(R.id.editTextText)
+        val emailEditText = findViewById<EditText>(R.id.editTextText2)
+        val comentarioEditText = findViewById<EditText>(R.id.editTextText3)
+        val enviarButton = findViewById<Button>(R.id.button)
+
+        enviarButton.setOnClickListener {
+            val nombre = nombreEditText.text.toString()
+            val email = emailEditText.text.toString()
+            val comentario = comentarioEditText.text.toString()
+
+            if (nombre.isEmpty() || email.isEmpty() || comentario.isEmpty()) {
+                Toast.makeText(this, "completa todos los campos.", Toast.LENGTH_SHORT).show()
+            } else {
+                val mensaje = """
+                    Nombre: $nombre
+                    Email: $email
+                    Comentario: $comentario
+                """.trimIndent()
+
+                Toast.makeText(this, mensaje, Toast.LENGTH_LONG).show()
+            }
+        }
+
+
     }
 }
